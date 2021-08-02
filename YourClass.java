32754ebbf6866aeca1476b28ebe3975a55820029
DTC-CS-Camp <68837059+DTC-CS-Camp@users.noreply.github.com>
2021-08-01 19:36:15 -0600

    Update YourClass.java

--- YourClass.java
+++ YourClass.java
@@ -1,1 +1,22 @@
 // create your class below! make sure to review your syntax and ask your volunteer/mentor for help.
+// below is just a reminder of the class, constructor, amd method syntax
+/* 
+modifier class className{ 
+  modifier className(parameters){
+
+  }
+  modifier void methodName( ){
+ 
+  }
+}
+*/
+
+
+
+
+
+
+
+
+
+
